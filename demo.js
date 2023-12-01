0b1285475cd2c209f2edebf4ba6d50cb6bd74b51
DLFlorencia <fdlazarte2@gmail.com>
2023-12-01 18:54:32 -0300

    primer intento

---
+++ demo.js
@@ -0,0 +1,47 @@
+variables
+
+var numero = 123;
+
+var nombre = "Alejandro";
+
+var Var = 23;
+
+operadores
+
+var resultado = 2 + 7;
+
+var resultadologico = (100 > 200) && (3 >= 3);
+
+metodos
+
+var nombre = "Alejo";
+
+Funciones
+
+function sumar(a,b) {
+var suma = a + b;
+return suma;
+}
+console.log(sumar(8,9));
+
+condicionales
+
+var edad = 15;
+if(edad>18) {
+console.log("soy mayor de edad");
+} else {
+console.log("soy menor de edad");
+}
+
+
+function dominioDelPais(dominio){
+    if(dominio==="co"){
+        console.log ("el pais es colombia");
+    }else if (dominio==="br"){
+        console.log("el pais es brasil");
+    }else{
+        console.log("el pais es argentina");
+    } 
+}
+
+dominioDelPais("br");
